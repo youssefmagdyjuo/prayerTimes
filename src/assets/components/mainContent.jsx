104b52a4c04a9c0c77dbb1105c66ff42709d8b80
Youssef Magdy <youssef.magdy.office@gmail.com>
2025-02-06 18:01:53 +0200

    Update mainContent.jsx

--- src/assets/components/mainContent.jsx
+++ src/assets/components/mainContent.jsx
@@ -5,31 +5,31 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { useEffect, useState } from 'react';
-import axios  from 'axios';
-import moment  from 'moment';
+import axios from 'axios';
+import moment from 'moment';
 import "moment/dist/locale/ar-ma"
 moment.locale("ar-ma")
-export default  function MainContent() {
+export default function MainContent() {
     // ______________ states _________________
     // avaliblcities state 
-    const [avaliblcities ,set_avaliblcities] = useState([
-        {nameEn:'Alexandria',nameAr:'الأسكندرية'},
-        {nameEn:'Cairo',nameAr:"القاهرة"},
-        {nameEn:'Ismailia',nameAr:"الاسماعيلية"},
-        {nameEn:'Gizeh',nameAr:"الجيزة"},
-        {nameEn:'Aswan',nameAr:"أسوان"},
-        {nameEn:'Luxor',nameAr:"الأقصر"},
-        {nameEn:'Damanhur',nameAr:"دمنهور"},
-        {nameEn:'Shubra El-Kheima',nameAr:"شبرا الخيمة"},
-        {nameEn:'Port Said',nameAr:"بور سعيد"},
+    const [avaliblcities, set_avaliblcities] = useState([
+        { nameEn: 'Alexandria', nameAr: 'الأسكندرية' },
+        { nameEn: 'Cairo', nameAr: "القاهرة" },
+        { nameEn: 'Ismailia', nameAr: "الاسماعيلية" },
+        { nameEn: 'Gizeh', nameAr: "الجيزة" },
+        { nameEn: 'Aswan', nameAr: "أسوان" },
+        { nameEn: 'Luxor', nameAr: "الأقصر" },
+        { nameEn: 'Damanhur', nameAr: "دمنهور" },
+        { nameEn: 'Shubra El-Kheima', nameAr: "شبرا الخيمة" },
+        { nameEn: 'Port Said', nameAr: "بور سعيد" },
     ])
     // country state 
-    const [country,setcountry]=useState({
-        countryEn:'EG',
-        countryAr:''
+    const [country, setcountry] = useState({
+        countryEn: 'EG',
+        countryAr: ''
     })
     // Timing State
-    const [timings,setTimings] = useState({
+    const [timings, setTimings] = useState({
         Fajr: "04:57",
         Dhuhr: "11:34",
         Asr: "14:30",
@@ -37,23 +37,23 @@ export default  function MainContent() {
         Isha: "18:20",
     })
     // City Stat (whitch city is choosen)
-    const [city,setCity]=useState({
-        nameAr:'',
-        nameEn:"Alexandria"
+    const [city, setCity] = useState({
+        nameAr: '',
+        nameEn: "Alexandria"
     })
     // time and day state 
-    const [toDay,setToDay]=useState('')
+    const [toDay, setToDay] = useState('')
     // timer state 
-    const [timer,setTimer]=useState("")
+    const [timer, setTimer] = useState("")
     // next prayer state 
-    const [nexPrayerIndex,setNextPrayerIndex]=useState(0)
+    const [nexPrayerIndex, setNextPrayerIndex] = useState(0)
     // All pryaers 
     const allPryers = [
-        {nameEn:'Fajr',nameAr:'الفجر'},
-        {nameEn:'Dhuhr',nameAr:"الظهر"},
-        {nameEn:'Asr',nameAr:"العصر"},
-        {nameEn:'Maghrib',nameAr:"المغرب"},
-        {nameEn:'Isha',nameAr:"العشاء"},
+        { nameEn: 'Fajr', nameAr: 'الفجر' },
+        { nameEn: 'Dhuhr', nameAr: "الظهر" },
+        { nameEn: 'Asr', nameAr: "العصر" },
+        { nameEn: 'Maghrib', nameAr: "المغرب" },
+        { nameEn: 'Isha', nameAr: "العشاء" },
     ]
     const allthing = [
         {
@@ -154,7 +154,7 @@ export default  function MainContent() {
             ]
         },
         {
-            countryEn: "PS", 
+            countryEn: "PS",
             countryAr: "فلسطين",
             capetal: { nameEn: "Jerusalem", nameAr: "القدس" },
             cities: [
@@ -169,55 +169,56 @@ export default  function MainContent() {
                 { nameEn: "Qalqilya", nameAr: "قلقيلية" }
             ]
         },
-        
-        
+
+
     ]
-    
+
     // new way to my website via location_______________________________________________________________new *
-    useEffect(()=>{
+    useEffect(() => {
         getLocation()
-    },[])
-    const getLocation = async()=>{
-        try{
-            const respons = await axios.get('https://ipapi.co/json') 
+    }, [])
+    const getLocation = async () => {
+        try {
+            const respons = await axios.get('https://ipapi.co/json')
             const data = respons.data
             // search location data in allthing array 
-            const userLocationobject = allthing.find((c)=>{return c.countryEn == data.country_code})
+            const userLocationobject = allthing.find((c) => { return c.countryEn == data.country_code })
             // if location is't included in allthing array 
-            if(userLocationobject==undefined){
-            setcountry({...country,countryEn:data.country_code,countryAr:data.country_name})
-            setCity({nameEn:data.city,nameAr: data.city})
+            if (userLocationobject == undefined) {
+                setcountry({ ...country, countryEn: data.country_code, countryAr: data.country_name })
+                setCity({ nameEn: data.city, nameAr: data.city })
             }
             // if location is included in allthing array 
-            else{
-            setcountry({...country,countryEn:userLocationobject.countryEn,countryAr:userLocationobject.countryAr})
-            set_avaliblcities(userLocationobject.cities)
-            // if city is't included in cities array 
-            const city_curentOpject = avaliblcities.find((city)=>{return city.nameEn == data.city})
-                if(city_curentOpject==undefined){
-                setCity({nameEn:data.city,nameAr: data.city})
+            else {
+                setcountry({ ...country, countryEn: userLocationobject.countryEn, countryAr: userLocationobject.countryAr })
+                set_avaliblcities(userLocationobject.cities)
+                // if city is't included in cities array 
+                const city_curentOpject = avaliblcities.find((city) => { return city.nameEn == data.city })
+                if (city_curentOpject == undefined) {
+                    setCity({ nameEn: data.city, nameAr: data.city })
                 }
                 // if city is included in cities array
-                else{
-                    setCity({nameEn:city_curentOpject.nameEn,nameAr:city_curentOpject.nameAr})
+                else {
+                    setCity({ nameEn: city_curentOpject.nameEn, nameAr: city_curentOpject.nameAr })
                 }
             }
-            
-        }catch(error){
-            alert(error)}
+
+        } catch (error) {
+            alert(error)
+        }
     }
     // handel coutry function
     const handelcoutry = (e) => {
-        const country_curentOpject = allthing.find((c)=>{
+        const country_curentOpject = allthing.find((c) => {
             return c.countryEn == e.target.value
         })
-        setcountry({...country,countryEn:country_curentOpject.countryEn,countryAr:country_curentOpject.countryAr})
+        setcountry({ ...country, countryEn: country_curentOpject.countryEn, countryAr: country_curentOpject.countryAr })
         set_avaliblcities(country_curentOpject.cities)
-        setCity({...city,nameEn:country_curentOpject.capetal.nameEn,nameAr:country_curentOpject.capetal.nameAr})
+        setCity({ ...city, nameEn: country_curentOpject.capetal.nameEn, nameAr: country_curentOpject.capetal.nameAr })
     };
 
     // api request ------------------------------------>
-    const getTimings = async()=>{
+    const getTimings = async () => {
         const respons = await axios.get(
             `https://api.aladhan.com/v1/timingsByCity?country=${country.countryEn}&city=${city.nameEn}`
         );
@@ -225,64 +226,60 @@ export default  function MainContent() {
         setTimings(respons.data.data.timings)
     }
     // api request by useEffect
-    useEffect(()=>{
+    useEffect(() => {
         getTimings();
-    },[city])
+    }, [city])
 
     // function handle selector to choose city
     const handlCity = (e) => {
-        const city_curentOpject = avaliblcities.find((city)=>{
+        const city_curentOpject = avaliblcities.find((city) => {
             return city.nameEn == e.target.value
         })
         setCity(city_curentOpject)
     };
     // useing timer 
-    useEffect(()=>{
-        let x = setInterval(()=>{
-        setUpCountDownTimer();
-        },1000);
+    useEffect(() => {
+        let x = setInterval(() => {
+            setUpCountDownTimer();
+        }, 1000);
         // refresh the time and day state  
-        const t =moment()
+        const t = moment()
         setToDay(t.format('Do MMM YYYY | hh:mm'))
-        return ()=>{
+        return () => {
             clearInterval(x)
         }
-    },[timings])
-    const setUpCountDownTimer = ()=>{
+    }, [timings])
+    const setUpCountDownTimer = () => {
         // time now 
-        const momentNow =moment();
+        const momentNow = moment();
         let nextPrayer = null;
         // cheack witch the next prayer is
-        if (momentNow.isAfter(moment(timings.Fajr,"hh:mm"))&&
-        momentNow.isBefore(moment(timings.Dhuhr,"hh:mm")))
-        {
-            nextPrayer =1
-        }else if (momentNow.isAfter(moment(timings.Dhuhr,"hh:mm"))&&
-        momentNow.isBefore(moment(timings.Asr,"hh:mm")))
-        {
-            nextPrayer =2  
-        }else if (momentNow.isAfter(moment(timings.Asr,"hh:mm"))&&
-        momentNow.isBefore(moment(timings.Maghrib,"hh:mm")))
-        {
-            nextPrayer =3
-        }else if (momentNow.isAfter(moment(timings.Maghrib,"hh:mm"))&&
-        momentNow.isBefore(moment(timings.Isha,"hh:mm")))
-        {
-            nextPrayer =4
-        }else {
-            nextPrayer =0
+        if (momentNow.isAfter(moment(timings.Fajr, "hh:mm")) &&
+            momentNow.isBefore(moment(timings.Dhuhr, "hh:mm"))) {
+            nextPrayer = 1
+        } else if (momentNow.isAfter(moment(timings.Dhuhr, "hh:mm")) &&
+            momentNow.isBefore(moment(timings.Asr, "hh:mm"))) {
+            nextPrayer = 2
+        } else if (momentNow.isAfter(moment(timings.Asr, "hh:mm")) &&
+            momentNow.isBefore(moment(timings.Maghrib, "hh:mm"))) {
+            nextPrayer = 3
+        } else if (momentNow.isAfter(moment(timings.Maghrib, "hh:mm")) &&
+            momentNow.isBefore(moment(timings.Isha, "hh:mm"))) {
+            nextPrayer = 4
+        } else {
+            nextPrayer = 0
         }
         setNextPrayerIndex(nextPrayer)
         // timer part with next pryer 
         const nextObject = allPryers[nextPrayer]
         const nextPrayerTime = timings[nextObject.nameEn]
-        const nextPrayerTimeMoment = moment(nextPrayerTime,"hh:mm")
+        const nextPrayerTimeMoment = moment(nextPrayerTime, "hh:mm")
         // الوقت الباقى 
         let remainingtTime = nextPrayerTimeMoment.diff(momentNow)
-        if (remainingtTime<0){
-            const midnigthDiff = moment("23:59:59","hh:mm:ss").diff(momentNow)
-            const faher_midnigthDiff = nextPrayerTimeMoment.diff(moment("00:00:00","hh:mm:ss"))
-            const totalDiff =midnigthDiff+ faher_midnigthDiff
+        if (remainingtTime < 0) {
+            const midnigthDiff = moment("23:59:59", "hh:mm:ss").diff(momentNow)
+            const faher_midnigthDiff = nextPrayerTimeMoment.diff(moment("00:00:00", "hh:mm:ss"))
+            const totalDiff = midnigthDiff + faher_midnigthDiff
             remainingtTime = totalDiff
         }
         // صيغة مفهومة 
@@ -291,72 +288,84 @@ export default  function MainContent() {
     }
     // ______________________________________________________Main Return_________________________________________________
     return (
-    <div>
-        {/* header  */}
-        <section className='header'>
+        <div>
+            {/* header  */}
+            <section className='header'>
                 <div className='header_rigth'>
                     <h2>{toDay}</h2>
                     <h1>{country.countryAr} | {city.nameAr}</h1>
                 </div>
                 <div className='header_left'>
                     <h2>متبقي حتى صلاة {allPryers[nexPrayerIndex].nameAr}</h2>
-                    <h1>{timer}</h1>                
+                    <h1>{timer}</h1>
                 </div>
-        </section>
-        {/* cards  */}
-        <Stack className='cards'>
-            <Prayer time={timings.Fajr}   name="الفجر" imge="./imgs/fgr.jpg"/>
-            <Prayer time={timings.Dhuhr}  name="الظهر" imge="./imgs/duhr.jpg"/>
-            <Prayer time={timings.Asr}    name="العصر" imge="./imgs/asr.jpg"/>
-            <Prayer time={timings.Maghrib}name="المغرب"imge="./imgs/mgrb.jpg"/>
-            <Prayer time={timings.Isha}   name="العشاء"imge="./imgs/isha.jpg"/>
-        </Stack>
-        {/* selecter  */}
-        <div className='selecters'>
-        <Stack  alignItems='center' style={{margin:'4rem auto'}}>
-            <FormControl className='select'>
-                <InputLabel id="demo-simple-select-label">
-                البلد
-                </InputLabel>
-                <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                onChange={handelcoutry}>
-                {
-                    // create <MenuItem> to every country in array (allthing)
-                    allthing.map((c,index)=>{
-                        return (
-                            <MenuItem key={index} value={c.countryEn}>{c.countryAr}</MenuItem>
-                        )
-                    })
-                }
-                </Select>
-            </FormControl>
-        </Stack>
-        
-        <Stack  alignItems='center' style={{margin:'4rem auto'}}>
-            <FormControl className='select'>
-                <InputLabel id="demo-simple-select-label">
-                المدينة
-                </InputLabel>
-                <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                onChange={handlCity}>
-                {
-                    // create <MenuItem> to every city in all cities array (avaliblcities)
-                    avaliblcities.map((city,index)=>{
-                        return (
-                            <MenuItem key={index} value={city.nameEn}>{city.nameAr}</MenuItem>
-                        )
-                    })
-                }
-                </Select>
-            </FormControl>
-        </Stack>
+            </section>
+            {/* cards  */}
+            <Stack className='cards'>
+                <Prayer time={timings.Fajr} name="الفجر" imge="./imgs/fgr.jpg" />
+                <Prayer time={timings.Dhuhr} name="الظهر" imge="./imgs/duhr.jpg" />
+                <Prayer time={timings.Asr} name="العصر" imge="./imgs/asr.jpg" />
+                <Prayer time={timings.Maghrib} name="المغرب" imge="./imgs/mgrb.jpg" />
+                <Prayer time={timings.Isha} name="العشاء" imge="./imgs/isha.jpg" />
+            </Stack>
+            {/* selecter  */}
+            <div className='selecters'>
 
+                <div>
+                    <label className="select" htmlFor="slct">
+                        <select id="slct" required
+                        onChange={handelcoutry}>
+                            <option disabled selected value="">
+                                البلد
+                            </option>
+                            {
+                                // create <MenuItem> to every country in array (allthing)
+                                allthing.map((c, index) => {
+                                    return (
+                                        <option key={index} value={c.countryEn}>{c.countryAr}</option>
+                                    )
+                                })
+                            }
+                        </select>
+                        <svg>
+                            <use xlinkHref="#select-arrow-down" />
+                        </svg>
+                    </label>
+                    <svg className="sprites">
+                        <symbol id="select-arrow-down" viewBox="0 0 10 6">
+                            <polyline points="1 1 5 5 9 1" />
+                        </symbol>
+                    </svg>
+                </div>
+                {/* city__________________________  */}
+                <div>
+                    <label className="select" htmlFor="slct">
+                        <select id="slct" required
+                        onChange={handlCity}>
+                            <option disabled selected value="">
+                                المدينة
+                            </option>
+                            {
+                                // create <MenuItem> to every city in all cities array (avaliblcities)
+                                avaliblcities.map((city, index) => {
+                                    return (
+                                        <option key={index} value={city.nameEn}>{city.nameAr}</option>
+                                    )
+                                })
+                            }
+                        </select>
+                        <svg>
+                            <use xlinkHref="#select-arrow-down" />
+                        </svg>
+                    </label>
+                    <svg className="sprites">
+                        <symbol id="select-arrow-down" viewBox="0 0 10 6">
+                            <polyline points="1 1 5 5 9 1" />
+                        </symbol>
+                    </svg>
+                </div>
 
+            </div>
         </div>
-    </div>
     )
 }
