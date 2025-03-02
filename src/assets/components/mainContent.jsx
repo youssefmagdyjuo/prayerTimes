06f27dc99cb080dc01d1e410f547139cb6462eea
Youssef Magdy <youssef.magdy.office@gmail.com>
2025-03-02 12:53:25 +0200

    Update mainContent.jsx

--- src/assets/components/mainContent.jsx
+++ src/assets/components/mainContent.jsx
@@ -55,6 +55,7 @@ export default function MainContent() {
     const [timer, setTimer] = useState("")
     // next prayer state 
     const [nexPrayerIndex, setNextPrayerIndex] = useState(0)
+    const [lastPrayerIndex, setLastPrayerIndex] = useState(0)
     // All pryaers 
     const allPryers = [
         { nameEn: 'Fajr', nameAr: 'الفجر' },
@@ -352,27 +353,36 @@ export default function MainContent() {
     const setUpCountDownTimer = () => {
         // time now 
         const momentNow = moment();
+
         let nextPrayer = null;
+        let lastPrayer = null;
         // cheack witch the next prayer is
         if (momentNow.isAfter(moment(timings.Fajr, "hh:mm")) &&
             momentNow.isBefore(moment(timings.Sunrise, "hh:mm"))) {
             nextPrayer = 1
+            lastPrayer =0
         } else if (momentNow.isAfter(moment(timings.Sunrise, "hh:mm")) &&
             momentNow.isBefore(moment(timings.Dhuhr, "hh:mm"))) {
             nextPrayer = 2
+            lastPrayer = 1
         } else if (momentNow.isAfter(moment(timings.Dhuhr, "hh:mm")) &&
             momentNow.isBefore(moment(timings.Asr, "hh:mm"))) {
             nextPrayer = 3
+            lastPrayer = 2
         } else if (momentNow.isAfter(moment(timings.Asr, "hh:mm")) &&
             momentNow.isBefore(moment(timings.Maghrib, "hh:mm"))) {
             nextPrayer = 4
+            lastPrayer = 3
         } else if (momentNow.isAfter(moment(timings.Maghrib, "hh:mm")) &&
             momentNow.isBefore(moment(timings.Isha, "hh:mm"))) {
             nextPrayer = 5
+            lastPrayer = 4
         } else {
             nextPrayer = 0
+            lastPrayer = 5
         }
         setNextPrayerIndex(nextPrayer)
+        setLastPrayerIndex(lastPrayer)
         // timer part with next pryer 
         // know what the next prayer is 
         const nextObject = allPryers[nextPrayer]
@@ -392,11 +402,17 @@ export default function MainContent() {
         const duration_remainingtTime = moment.duration(remainingtTime)
         setTimer(`${duration_remainingtTime.seconds()} : ${duration_remainingtTime.minutes()} : ${duration_remainingtTime.hours()}`)
         // know what the last prayer is 
-        const lastObject = allPryers[nextPrayer-1]
+        const lastObject = allPryers[lastPrayer]
         const lastPrayerTime = timings[lastObject.nameEn]
         const lastPrayerTimeMoment = moment(lastPrayerTime, "hh:mm")
         // الوقت المتبقي بين الصلاة السابقة والصلاة القادمة
         let diff_time = nextPrayerTimeMoment.diff(lastPrayerTimeMoment)
+        if (diff_time < 0) {
+            const midnigthDiff = moment("23:59:59", "hh:mm:ss").diff(lastPrayerTimeMoment)
+            const faher_midnigthDiff = nextPrayerTimeMoment.diff(moment("00:00:00", "hh:mm:ss"))
+            const totalDiff = midnigthDiff + faher_midnigthDiff
+            diff_time = totalDiff
+        }
         // but the diff between last prayer and next prayer as seconds in state to use it in GUI 
         setTheDiff_between_2prayer(diff_time)
     }
